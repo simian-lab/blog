969d50d64020eeb7b00477853cfa6077817f86a8
Pablo <pablo@puntomob.com>
2014-05-02 19:09:23 -0500

    [BLOG-43]

--- app/wp-content/themes/syntax-simian/footer.php
+++ app/wp-content/themes/syntax-simian/footer.php
@@ -16,7 +16,7 @@
 			</div>
 		<?php endif; ?>
 		<div class="site-info">
-			Except as otherwise noted, the content of this page is licensed under the Creative Commons Attribution 3.0 License, and code samples are licensed under the Apache 2.0 License.
+			<?php _e( 'Except as otherwise noted, the content of this page is licensed under the Creative Commons Attribution 3.0 License, and code samples are licensed under the Apache 2.0 License.', 'syntax' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
