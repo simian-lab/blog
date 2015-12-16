5208a53d80c9ef04bc443e6c334028c8d9dd0ce2
PabloAlejandroGarcia <pablo@simian.co>
2015-12-16 18:15:07 -0500

    BLOG-75 More of the home align

--- app/wp-content/themes/simian-remake/footer.php
+++ app/wp-content/themes/simian-remake/footer.php
@@ -1,3 +1,23 @@
+  <footer class="footer">
+  	<div class="content">
+  		<div class="links block">
+  			<h2 class="title"><?php _e('Contenido', 'simian_theme'); ?></h2>
+  		</div>
+  		<div class="info block">
+  			<h2 class="title"><?php _e('Información', 'simian_theme'); ?></h2>
+  		</div>
+  		<div class="social block">
+  			<h2 class="title"><?php _e('Síguenos', 'simian_theme'); ?></h2>
+  		</div>
+	  	<div class="copyright">
+	  		<p><?php _e('©2016 Simian todos los derechos reservados', 'simian_theme'); ?></p>
+	  		<p>
+	  			<?php _e('A menos que se señale lo contrario, el contenido de esta página este licenciado bajo la licencia Creative Commons Attribution 3.0', 'simian_theme');
+	  			_e(', y los ejemplos de código están registrados bajo la licencia 2.0 de Apache.', 'simian_theme'); ?>
+	  		</p>
+	  	</div>
+  	</div>
+  </footer>
   <?php wp_footer(); ?>
 </body>
 </html>
--- app/wp-content/themes/simian-remake/index.php
+++ app/wp-content/themes/simian-remake/index.php
@@ -15,4 +15,53 @@
  */
 
 get_header();
-get_footer();
+//Main query 
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+$args = array(
+  'order' => 'DESC',
+  'orderby' => 'date',
+  'post_status' => 'publish',
+  'posts_per_page' => 4,
+  'paged' => $paged
+
+);
+$the_query = new WP_Query( $args );
+?>
+<div class="main">
+	<div class="content">
+		<?php if ( $the_query->have_posts() ): ?>
+			<div class="posts-list">
+				<?php while ( $the_query->have_posts() ):
+				$the_query->the_post(); ?>
+				<div class="post-container">
+					<a href="<?php the_permalink(); ?>">
+						<?php the_post_thumbnail('large'); ?>
+						<h2 class="post-title">
+							<?php the_title(); ?>
+						</h2>
+						<p class="post-excerpt">
+							<?php the_excerpt(); ?>
+						</p>
+					</a>
+					<div class="post-foot">
+						<div class="date">
+							<p class="month"><?php the_date('j M'); ?></p>
+							<?php echo __(' de ','simian_theme') . get_the_date('Y'); ?>
+						</div>
+						<div class="author">
+							<?php the_author_meta('first_name'); echo ' '; the_author_meta('last_name') ; ?>
+						</div>
+						<div class="comments">
+							<?php echo __('Comentarios', 'simian_theme') . '('.wp_count_comments()->total_comments . ')'; ?>
+						</div>
+						<div class="tags">
+							<?php the_tags(''); ?>
+						</div>
+					</div>					
+				</div>
+				<?php endwhile; ?>
+			</div>
+		<?php endif; ?>
+	</div>
+</div>
+<?php get_footer();
