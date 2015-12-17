25c978a17e6f5e030c4ec2cc539cba1b81b16f9c
PabloAlejandroGarcia <pablo@simian.co>
2015-12-17 16:53:01 -0500

    BLOG-75 Logos y fuente de estilos

--- app/wp-content/themes/simian-remake/header.php
+++ app/wp-content/themes/simian-remake/header.php
@@ -11,13 +11,14 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width">
+  <link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Quicksand:400,700' rel='stylesheet' type='text/css'>
   <title><?php wp_title( '|', true, 'right' ); ?></title>
   <?php wp_head(); ?>
 </head>
 <body id="page" <?php body_class(); ?>>
 	<header class="header">
 		<div class="top-header">
-			<img class="logo" src="" alt="Simian Logo">
+			<img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Simian Logo">
 			<nav class="menu">
 				<?php
         $args = array(
@@ -38,6 +39,6 @@
 			<h1 class="site-title">
 				<?php _e('Nuestro Blog', 'simian_theme'); ?>
 			</h1>
-			<img class="talking-monkey" src="" alt="Mono Aullador">
+			<img class="talking-monkey" src="<?php echo get_template_directory_uri(); ?>/images/talking-monkey.svg" alt="Mono Aullador">
 		</div>
 	</header>
