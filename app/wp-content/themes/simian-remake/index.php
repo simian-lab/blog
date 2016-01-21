520c2beb92aa8f6797dc6de5832f483a656d5d49
PabloAlejandroGarcia <pablo@simian.co>
2016-01-21 10:00:59 -0500

    BLOG-75 Fixes to pagination, sizes, etc

--- app/wp-content/themes/simian-remake/index.php
+++ app/wp-content/themes/simian-remake/index.php
@@ -77,8 +77,9 @@ query_posts( $args );
 				<div class="pagination">
 					<?php
 					$pagination_args = array(
-						'show_all' => True,
-        		'mid_size' => 2,
+						'show_all' => False,
+        		'mid_size' => 1,
+        		'end_size' => 1,
         		'next_text' => '<svg class="icon icon-right-arrow-circular"><use xlink:href="#icon-right-arrow-circular"></use></svg><span class="mls"></span>',
         		'prev_text' => '<svg class="icon icon-left-arrow-circular"><use xlink:href="#icon-left-arrow-circular"></use></svg><span class="mls"></span>',
         		'type' => 'plain',
