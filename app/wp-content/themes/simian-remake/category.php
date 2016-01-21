a8e74e8863367da6559914d0c0a11b0ac02f9230
PabloAlejandroGarcia <pablo@simian.co>
2016-01-21 15:28:23 -0500

    BLOG-76 Galleries margin, fixes to post image and more

--- app/wp-content/themes/simian-remake/category.php
+++ app/wp-content/themes/simian-remake/category.php
@@ -83,7 +83,7 @@ global $wp_query;?>
 						'show_all' => False,
         		'mid_size' => 1,
         		'end_size' => 1,
-        		'total' => $the_query->max_num_pages,
+        		'total' => $wp_query->max_num_pages,
         		'next_text' => '<svg class="icon icon-right-arrow-circular"><use xlink:href="#icon-right-arrow-circular"></use></svg><span class="mls"></span>',
         		'prev_text' => '<svg class="icon icon-left-arrow-circular"><use xlink:href="#icon-left-arrow-circular"></use></svg><span class="mls"></span>',
         		'type' => 'plain',
