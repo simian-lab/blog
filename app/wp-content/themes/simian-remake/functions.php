b95e3a27ac272b5ae73a168436e05ad241430170
PabloAlejandroGarcia <pablo@simian.co>
2016-01-22 11:13:35 -0500

    Border from related fixed for new margin

--- app/wp-content/themes/simian-remake/functions.php
+++ app/wp-content/themes/simian-remake/functions.php
@@ -47,7 +47,7 @@ if ( ! function_exists( 'simian_theme_enqueue_scripts' ) ) :
 
     
     //Styles :D
-    wp_enqueue_style('simian-style', get_stylesheet_directory_uri() . '/css/styles.css', '', '2.2.7');
+    wp_enqueue_style('simian-style', get_stylesheet_directory_uri() . '/css/styles.css', '', '2.2.8');
    
   }
 endif;
