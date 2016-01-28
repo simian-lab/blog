7d19cc4f36352b70b7587bd3ae10afcc53c0b3d5
PabloAlejandroGarcia <pablo@simian.co>
2016-01-28 15:20:56 -0500

    Header single working

--- app/stack/openshift-dev-config.php
+++ app/stack/openshift-dev-config.php
@@ -78,7 +78,7 @@ define('WPLANG', 'es_ES');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 /* Deactivate WordPress automatic updates */
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
