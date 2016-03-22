465d3544899137ff8d058176a01459e13a6eec86
PabloAlejandroGarcia <pablo@simian.co>
2016-03-22 10:53:28 -0500

    BLOG-133 falta la transicion al regresar al top

--- app/wp-content/themes/simian-remake/functions.php
+++ app/wp-content/themes/simian-remake/functions.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'simian_theme_enqueue_scripts' ) ) :
 
     
     //Styles :D
-    wp_enqueue_style('simian-style', get_stylesheet_directory_uri() . '/css/styles.css', '', '2.6.7');
+    wp_enqueue_style('simian-style', get_stylesheet_directory_uri() . '/css/styles.css', '', '2.6.8');
    
   }
 endif;
